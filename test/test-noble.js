1d6585625812380f67f6721477a10966065646cc
Scott Leibrand <scottleibrand@gmail.com>
2026-07-19 17:06:32 -0700

    Log known-peripheral connect failures

--- test/test-noble.js
+++ test/test-noble.js
@@ -5,6 +5,32 @@ var sinon = require('sinon');
 var Noble = require('../lib/noble');
 
 describe('Noble', function() {
+  it('reports known-peripheral connect errors before the callback can ignore them', function() {
+    var noble = new Noble(new events.EventEmitter());
+    var warning = null;
+    var error = new Error('Command Disallowed (0xc)');
+    error.hciStatus = 12;
+    error.hciStatusHex = '0x0c';
+    error.hciDeviceId = 0;
+    var peripheral = new events.EventEmitter();
+    peripheral.state = 'connecting';
+    noble._peripherals['mock-peripheral'] = peripheral;
+    noble.logAdapterFlagsForConnectFailure = sinon.spy();
+    noble.on('warning', function(message) {
+      warning = message;
+    });
+
+    noble.onConnect('mock-peripheral', error);
+
+    warning.should.equal(
+      'connect failed peripheral=mock-peripheral ' +
+      'error=Command Disallowed (0xc) hciStatus=0x0c'
+    );
+    peripheral.state.should.equal('error');
+    noble.logAdapterFlagsForConnectFailure.calledOnce.should.equal(true);
+    noble.logAdapterFlagsForConnectFailure.calledWithExactly(error).should.equal(true);
+  });
+
   it('reports unknown-peripheral connect errors with numeric HCI status', function() {
     var noble = new Noble(new events.EventEmitter());
     var warning = null;
@@ -20,7 +46,7 @@ describe('Noble', function() {
     noble.onConnect('mock-peripheral', error);
 
     warning.should.equal(
-      'unknown peripheral mock-peripheral connected! ' +
+      'connect failed peripheral=mock-peripheral ' +
       'error=Command Disallowed (0xc) hciStatus=0x0c'
     );
     noble.logAdapterFlagsForConnectFailure.calledOnce.should.equal(true);
